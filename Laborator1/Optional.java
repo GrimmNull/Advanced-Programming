1088744612de7a0536c61edd5da28313e459913b
Mr. Grimm <30557503+ZuraJanaiKatsuraDa@users.noreply.github.com>
2021-02-21 19:14:47 +0200

    Rezolvat cateva erori + optimizari

--- Laborator1/Optional.java
+++ Laborator1/Optional.java
@@ -1,19 +1,16 @@
-import java.util.Random;
 import java.util.Arrays;
 
 public class Optional {
     public static boolean[][] matriceAdiacenta, matriceArbore;
     public static Integer[] vectorVizite,vectorOrdineVizite;
-    public static int n,indexVizita=1,indexComponenta=1, urmatorulNod=0;
+    public static int n,indexComponenta=1, urmatorulNod=0;
     public static boolean outputLog=true;
     public static void DFS(int nodCurent)
     {
             vectorVizite[nodCurent]=indexComponenta;
-            vectorOrdineVizite[nodCurent]=indexVizita;
-            indexVizita=indexVizita+1;
             for(int i=0;i<n;i++){
-                if(matriceAdiacenta[nodCurent][i] && vectorVizite[i]!=1){
-                    matriceArbore[nodCurent][i]=true;
+                if(matriceAdiacenta[nodCurent][i] && vectorVizite[i]==0){
+                    matriceArbore[i][nodCurent]=matriceArbore[nodCurent][i]=true;
                     DFS(i);
                     
                 }
@@ -28,19 +25,18 @@ public class Optional {
             System.out.println("Argumentul nu este valid!");
             return;
         }
-        System.out.println(n);
-        if(n>30_000){
+        if(n>=1_000){
             outputLog=false;
         }
-        Random rand= new Random();
-        boolean existaElementNevizitat,existaComponenteConexe;
+        boolean existaElementNevizitat;
         matriceAdiacenta=new boolean[n][n];
         matriceArbore=new boolean[n][n];
         vectorVizite=new Integer[n];
+        Arrays.fill(vectorVizite,0);
         vectorOrdineVizite=new Integer[n];
         for(int i=0;i<n;i++) {
             for(int j=i;j<n;j++) {
-                matriceAdiacenta[i][j]=(rand.nextInt(2)==1 ? true : false);
+                matriceAdiacenta[i][j]=Math.random()<2.0f/n;
                 matriceAdiacenta[j][i]=matriceAdiacenta[i][j];
             }
         }
@@ -64,8 +60,7 @@ public class Optional {
         }while(existaElementNevizitat!=false);
 
 
-        existaComponenteConexe= Arrays.stream(vectorVizite).anyMatch(x -> x == 2);
-        if(existaComponenteConexe==true){
+        if(indexComponenta>1){
             if(outputLog==true)
             {
                 System.out.println("Graful nu este conex");
