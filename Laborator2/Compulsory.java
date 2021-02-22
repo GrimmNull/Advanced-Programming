abdb93ede48d2d25faec2081e2865affba457599
Mr. Grimm <30557503+ZuraJanaiKatsuraDa@users.noreply.github.com>
2021-02-22 09:29:09 +0200

    Resolved the compulsory exercises

---
+++ Laborator2/Compulsory.java
@@ -0,0 +1,165 @@
+public class Laborator2{
+
+    public enum SourceType {
+        WAREHOUSE, FACTORY;
+    }
+
+    public static class Solution{
+        String name;
+        int supply;
+        SourceType type;
+
+        public Solution(){
+            name=new String("default");
+            type=SourceType.FACTORY;
+            supply=-1;
+        }
+
+        public Solution(String nume, SourceType tipul, int cantitate){
+            name=new String(nume);
+            type=tipul;
+            supply=cantitate;
+        }
+
+        public String toString(){
+            return String.format("Sursa " + name + " de tipul " + type + " care are " + supply + " resurse");
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public SourceType getType(){
+            return type;
+        }
+
+        public int getSupply(){
+            return supply;
+        }
+
+        public void setName(String nume){
+            name=nume;
+        }
+        public void setType(SourceType tip){
+            type=tip;
+        }
+
+        public void setSupply(int cantitate){
+            supply=cantitate;
+        }
+    }
+
+    public static class Destination{
+        String name;
+        int demand;
+
+        public Destination(){
+            name=new String("default");
+            demand=-1;
+        }
+
+        public Destination(String nume, int cerere){
+            name=new String(nume);
+            demand=cerere;
+        }
+
+        public String toString(){
+            return String.format("Destinatia " + name + " care are cererea de " + demand + " resurse");
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getDemand(){
+            return demand;
+        }
+
+        public void setName(String nume){
+            name=nume;
+        }
+
+        public void setDemand(int cerere){
+            demand=cerere;
+        }
+
+    }
+
+    public static class Problem{
+        Solution sol;
+        Destination dest;
+        int cost;
+
+        public Problem(String numeSol, SourceType tipSol, int cantitateSol, String numeDest, int cerereDest, int costProblema){
+            sol= new Solution(numeSol,tipSol,cantitateSol);
+            dest=new Destination(numeDest,cerereDest);
+            cost=costProblema;
+        }
+
+        public Problem(Solution solutie, Destination destinatie, int costProblema){
+            sol=solutie;
+            dest=destinatie;
+            cost=costProblema;
+        }
+
+        public String toString(){
+            return String.format("Sursa " + sol.name + " de tipul " + sol.type + " care are " + sol.supply + " resurse, poate trimite catre destinatia " +  dest.name + ", care are cererea de " + dest.demand + " cu un cost de " + cost);
+        }
+
+    }
+
+    public static void main(String[] args){
+        //instantam solutiile din problema data in exemplu
+            Solution sol1=new Solution("S1",SourceType.FACTORY,10);
+            Solution sol2=new Solution("S2",SourceType.WAREHOUSE,35);
+            Solution sol3=new Solution("S3",SourceType.WAREHOUSE,25);
+
+
+        //instantam destinatiile din problema
+            Destination dest1=new Destination("D1",20);
+            Destination dest2=new Destination("D2",25);
+            Destination dest3=new Destination("D3",25);
+
+
+        //instantam obiectele de tip Problem pe care le vom folosi la afisarea matricii
+            Problem[][] prb=new Problem[3][3];
+            prb[0][0]=new Problem(sol1,dest1,2);
+            prb[0][1]=new Problem(sol1,dest2,3);
+            prb[0][2]=new Problem(sol1,dest3,1);
+            prb[1][0]=new Problem(sol2,dest1,5);
+            prb[1][1]=new Problem(sol2,dest2,4);
+            prb[1][2]=new Problem(sol2,dest3,8);
+            prb[2][0]=new Problem(sol3,dest1,5);
+            prb[2][1]=new Problem(sol3,dest2,6);
+            prb[2][2]=new Problem(sol3,dest3,8);
+
+        //formatare pentru aspect si afisam numele destinatiilor    
+            System.out.print("   ");
+            for(int i=0;i<3;i++){
+                System.out.print(prb[0][i].dest.name + " ");
+            }
+
+        //afisam costurile problemelor
+            System.out.println();
+            for(int i=0;i<3;i++){
+                System.out.print(prb[i][0].sol.name + " ");
+                for(int j=0;j<3;j++){
+                    System.out.print(prb[i][j].cost + "  ");
+                }
+                System.out.print(prb[i][0].sol.supply);
+                System.out.println();
+            }
+            
+        //afisam cantitatile necesare
+            System.out.print("   ");
+            for(int i=0;i<3;i++){
+                System.out.print(prb[0][i].dest.demand + " ");
+            }
+
+        //afisam cate un obiect din fiecare clasa pentru a demonstra suprascrierea functiei toString
+            System.out.println();
+            System.out.println(sol1);
+            System.out.println(dest1);
+            System.out.println(prb[0][0]);
+    }
+}
